29d22b1daf4176d04c4a9b388c3f877dca2babbb
Marcin Głowacki <33039258+M0arcin@users.noreply.github.com>
2020-04-23 18:48:38 +0200

    Update test.java

--- src/main/java/Client/Test.java
+++ src/main/java/Client/Test.java
@@ -9,8 +9,6 @@ public class Test {
 
         JsonPrinter printer = new JsonPrinter();
 
-        //so far it works only multiple mustMatch/shouldMatch
-
         //Query example1 = new Query.QueryBuilder().bool().mustMatch("item", "Milk").mustMatch("price", "23").shouldMatch("product_location", "New Mexico").bool().shouldMatch("warranty", "2 years").shouldMatch("seller", "to warzywo").build();
         //printer.printJson(example1);
 
